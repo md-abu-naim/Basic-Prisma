bfb081a308c05d4f0759b76153224511151be17a
abunaim34 <farhanadnanfarabi@gmail.com>
2026-06-18 21:59:07 +0600

    my first prisma query

---
+++ src/script.ts
@@ -0,0 +1,41 @@
+import { prisma } from "./lib/prisma";
+
+async function main() {
+    // Create a new user with a post
+
+    const user = await prisma.user.create({
+        data: {
+            name: "Aliac1",
+            email: 'aliac1@gmail.com',
+            posts: {
+                create: {
+                    title: "Hello World 1",
+                    content: "This is my second posts!",
+                    published: true,
+                }
+            }
+        },
+        include: {
+            posts: true,
+        }
+    })
+    console.log("Created user:", user);
+
+    // Fetch all users with their posts
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+        },
+    });
+    console.log("All users:", JSON.stringify(allUsers, null, 2));
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
